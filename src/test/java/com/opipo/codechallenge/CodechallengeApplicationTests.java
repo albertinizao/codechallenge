92dbc9bdcd7cfba1e5918e4da8be539d772cc3c0
acebrian <alberto.cebrian@atmira.com>
2019-12-05 18:33:33 +0100

    Base springboot project

---
+++ src/test/java/com/opipo/codechallenge/CodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opipo.codechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
